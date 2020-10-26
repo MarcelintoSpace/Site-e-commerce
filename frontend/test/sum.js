7fb472eb19ea8538accbe81c52e537d003a07315
MarcleintoSpace <marcelintospace@gmail.com>
2020-10-26 09:51:33 +0100

    commit mentorat

---
+++ frontend/test/sum.js
@@ -0,0 +1,24 @@
+let request = obj => {
+  return new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open(obj.method || "GET", obj.url);
+    if (obj.headers) {
+      Object.keys(obj.headers).forEach(key => {
+        xhr.setRequestHeader(key, obj.headers[key]);
+      });
+    }
+
+// vérification de la réponse de l'API
+    xhr.onload = () => {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr.response);
+      } else {
+        reject(xhr.statusText);
+      }
+    };
+    xhr.onerror = () => reject(xhr.statusText);
+    xhr.send(obj.body);
+  });
+};
+
+module.exports = sum;
